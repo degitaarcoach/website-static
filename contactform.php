f09da03e56b123bd021417e09a7283f4b2ec48a5
Corne <degitaarcoach@gmail.com>
2021-03-13 11:23:18 +0100

    added working contactform with php

--- contactform.php
+++ contactform.php
@@ -1,45 +1,47 @@
-<html>
+<!DOCTYPE html>
+
+<?php 
+  $message_sent = false;
+
+  if(isset($_POST['email']) && $_POST['email'] != '') {
+    if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+      $userName = secure_input($_POST['name']);
+      $userEmail = secure_input($_POST['email']);
+      $subject = secure_input($_POST['subject']);
+      $message = secure_input($_POST['message']);
+    
+      $to = "degitaarcoach@gmail.com";
+      $body = "";
+    
+      $body .= "From: ".$userName. "\r\n";
+      $body .= "Email: ".$userEmail. "\r\n";
+      $body .= "Message: ".$message. "\r\n";
+    
+      mail($to, $subject, $body);
+
+      $message_sent = true;
+    }
+  }
+
+  function secure_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  ?>
+
+  <html lang="nl">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bedankt voor je bericht!</title>
+  </head>
   <body>
-
-  <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $messageErr = "";
-$name = $email = $subject = $message = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  if (empty($_POST["name"])) {
-    $nameErr = "Vul eerst uw naam in";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
-
- if (empty($_POST["email"])) {
-    $emailErr = "Vul eerst uw email adres in";
-  } else {
-    $email = test_input($_POST["email"]);
-  } 
-
-  if (empty($_POST["subject"])) {
-    $subject = "";
-  } else {
-    $subject = test_input($_POST["subject"]);
-  }
-
-  if (empty($_POST["message"])) {
-    $emailErr = "Vul eerst uw bericht in";
-  } else {
-    $email = test_input($_POST["message"]);
-  } 
-
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
+    <h1>Bedankt voor je mail</h1>
+    <a href="index.html">Ga Terug</a>
   </body>
-</html>
+  </html>
